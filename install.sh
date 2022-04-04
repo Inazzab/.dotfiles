ea6810c9a4f73e50bf49b07f7bf1b44e6dc903f3
Inazzab <97700961+Inazzab@users.noreply.github.com>
2022-04-03 22:15:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo pacman -Syu --needed base-devel git
 git clone https://aur.archlinux.org/yay
 cd yay
 makepkg -si
-yay -S brave librewolf bitwarden neovim onlyoffice
+yay -S brave librewolf bitwarden neovim neovim-plug onlyoffice
