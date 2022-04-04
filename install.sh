3bed42fb64d4b88006cff3f3510a9d67a94ea16a
Inazzab <97700961+Inazzab@users.noreply.github.com>
2022-04-04 17:36:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo pacman -Syu --needed base-devel git
+cd ~
 git clone https://aur.archlinux.org/yay
 cd yay
 makepkg -si
 yay -S brave librewolf bitwarden neovim neovim-plug onlyoffice nvidia
+ntpd -qg #Resynchronizes clock in case of timezone issues when dual booting w/windows
