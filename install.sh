2115b9cb05f4ab1ad95a8766033dd791c5e3ec9a
Inazzab <97700961+Inazzab@users.noreply.github.com>
2022-04-04 17:53:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ git clone https://aur.archlinux.org/yay
 cd yay
 makepkg -si
 yay -S brave librewolf bitwarden neovim neovim-plug onlyoffice nvidia
+cd .config && mkdir nvim
+cp ~/.dotfiles/init.vim .config/nvim/init.vim
 ntpd -qg #Resynchronizes clock in case of timezone issues when dual booting w/windows
