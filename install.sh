f6d160449b24e1fb6e6e839ee195deaeef998724
Inazzab <97700961+Inazzab@users.noreply.github.com>
2022-04-04 18:26:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+#Install yay
 sudo pacman -Syu --needed base-devel git
 cd ~
 git clone https://aur.archlinux.org/yay
 cd yay
 makepkg -si
-yay -S brave librewolf bitwarden neovim neovim-plug onlyoffice nvidia
+
+#Enable arch repos
+yay -S artix-archlinux-support
+cp ~/.dotfiles/pacman.conf /etc/pacman.conf
+pacman-key --populate archlinux
+
+yay -S brave librewolf bitwarden neovim neovim-plug onlyoffice nvidia xmonad xmonad-contrib
 cd .config && mkdir nvim
 cp ~/.dotfiles/init.vim .config/nvim/init.vim
 ntpd -qg #Resynchronizes clock in case of timezone issues when dual booting w/windows
